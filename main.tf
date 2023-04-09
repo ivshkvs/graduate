b610163d4a467860c67f25f478c4fe0fcc37a16d
Saveli Ivashkov <ivshkvs@gmail.com>
2023-04-09 16:31:53 +0300

    teratest

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair"
+  subnet_id     = "subnet-12345678"
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
