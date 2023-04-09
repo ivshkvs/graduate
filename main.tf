a1af607aab4ea4841a4a731dbf1090dc4ac72114
Saveli Ivashkov <ivshkvs@gmail.com>
2023-04-09 16:46:23 +0300

    teratest

--- main.tf
+++ main.tf
@@ -1,22 +1,62 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  key_name      = "my-key-pair"
-  subnet_id     = "subnet-12345678"
-}
-
-resource "aws_security_group" "example" {
-  name_prefix = "example"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  instance_type          = var.server_size
+  vpc_security_group_ids = [aws_security_group.web.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>${var.server_name}-WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+  tags = {
+    Name  = "${var.server_name}-WebServer"
+    Owner = "Denis Astahov"
+  }
+}
+
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
+resource "aws_security_group" "web" {
+  name_prefix = "${var.server_name}-WebServer-SG"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "${var.server_name}-WebServer SecurityGroup"
+    Owner = "Denis Astahov"
+  }
+}
+
+resource "aws_eip" "web" {
+  vpc      = true # Need to add in new AWS Provider version
+  instance = aws_instance.web.id
+  tags = {
+    Name  = "${var.server_name}-WebServer-IP"
+    Owner = "Denis Astahov"
+  }
+}
