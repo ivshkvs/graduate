23f33a9f9087b2642b7e2b510b2afda4009974f2
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-10 16:13:09 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,9 @@ resource "aws_instance" "bot" {
   ami                    = data.aws_ami.latest_amazon_linux.id
   instance_type          = var.server_size
   vpc_security_group_ids = [aws_security_group.bot.id]
-  create_before_destroy  = true
+  lifecycle {
+    create_before_destroy = true
+  }
   user_data	= file("docker.sh")
 
   tags = {
