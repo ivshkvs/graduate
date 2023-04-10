5cb697fe8ad0657b147e7ac727774971b1601726
Saveli Ivashkov <ivshkvs@gmail.com>
2023-04-10 12:52:55 +0300

    docker install

--- terraform/sh/docker.sh
+++ terraform/sh/docker.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-sudo apt update \
-&& sudo apt install apt-transport-https ca-certificates curl software-properties-common \
-&& curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
-&& sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" \
-&& apt-cache policy docker-ce \
-&& sudo apt install docker-ce \
-&& sudo usermod -aG docker ${ubuntu} \
-&& su - ${ubuntu} \
-&& sudo usermod -aG docker ubuntu
-
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+docker --version
