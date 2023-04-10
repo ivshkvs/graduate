f0ff6cbef6fd04881fa31174623d9686b6587eaf
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-10 13:12:01 +0300

    Update docker.sh

--- terraform/sh/docker.sh
+++ terraform/sh/docker.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install docker.io -y
+
+# Install Docker
+yum update -y
+yum install docker -y
+
+# Start Docker and configure it to start on boot
+systemctl start docker
+systemctl enable docker
+
+# Add the current user to the docker group to avoid using sudo every time
+usermod -aG docker $USER
