905b07dd64e2dcd93d0e4ab2783f1cbf127cddbe
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-10 16:01:35 +0300

    do

--- terraform/docker.sh
+++ terraform/docker.sh
@@ -12,6 +12,10 @@ sudo yum install -y git
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
-sudo docker login -u ivshkvs -p dckr_pat_w1f9n80eI3PRBXwH_oQZszObTPQ \
-&& sudo docker pull ivshkvs/tg_bot:latest \
-&& sudo docker run -d ivshkvs/tg_bot:latest
+while true
+do
+ sudo docker login -u ivshkvs -p dckr_pat_w1f9n80eI3PRBXwH_oQZszObTPQ \
+ && sudo docker pull ivshkvs/tg_bot:latest \
+ && sudo docker run -d ivshkvs/tg_bot:latest
+ sleep 60
+done
