848b12d1c80d3b9220f2e5ba31d27ef295f13619
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-10 15:16:09 +0300

    timestamp

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "bot" {
   user_data	= file("docker.sh")
 
   tags = {
-    Name  = "${var.server_name}server003"
+    Name  = "${var.server_name}server_${timestamp()}"
     Owner = "Saveli Ivashkov"
   }
 }
