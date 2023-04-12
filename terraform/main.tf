ecad3e5f02f93e9159085cb4fdc817030eeb139c
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-12 22:58:25 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,14 @@ resource "aws_security_group" "bot" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
