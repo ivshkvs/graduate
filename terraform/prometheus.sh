10d86ebfad978a5771d1ab84b46ade62e6400db5
Saveli Ivashkov <ivshkvs@gmail.com>
2023-04-13 18:22:02 +0300

    prom

---
+++ terraform/prometheus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo useradd --no-create-home prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.19.0/prometheus-2.19.0.linux-amd64.tar.gz
+tar xvfz prometheus-2.19.0.linux-amd64.tar.gz
+
+sudo cp prometheus-2.19.0.linux-amd64/prometheus /usr/local/bin
+sudo cp prometheus-2.19.0.linux-amd64/promtool /usr/local/bin/
+sudo cp -r prometheus-2.19.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.19.0.linux-amd64/console_libraries /etc/prometheus
+
+sudo cp prometheus-2.19.0.linux-amd64/promtool /usr/local/bin/
+rm -rf prometheus-2.19.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+echo "global:
+  scrape_interval: 15s
+  external_labels:
+    monitor: 'prometheus'
+
+scrape_configs:
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090']" > /etc/prometheus/prometheus.yml
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus 
