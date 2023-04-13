6c1cfce1bca7038a5000e36fac66996dd618b945
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-13 18:31:13 +0300

    eof

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,11 @@ resource "aws_instance" "bot" {
   lifecycle {
     create_before_destroy = true
   }
-  user_data	= "${file("docker.sh")} + "${file("prometheus.sh")}"
-
+  user_data	= <<-EOF
+  ${file("docker.sh")}
+  ${file("prometheus.sh")}
+  EOF
+  
   tags = {
     Name  = "${var.server_name}server_${timestamp()}"
     Owner = "Saveli Ivashkov"
