e26c6bbdb8ba352d4f738dc1268db01467a0b29d
Saveli Ivashkov <113042891+ivshkvs@users.noreply.github.com>
2023-04-13 15:27:07 +0300

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -61,7 +61,7 @@ def handle_date(message):
 
 # Launch bot
 if __name__ == '__main__':
-    start_http_server(8001)
+    start_http_server(8000)
     while True:
         process_request(random.random())
     bot.polling()
